351ac6479d8a1a83457170521c25c4859944128d
Bautista Mangoni <jmangoni@asince.com.ar>
2024-10-18 17:50:06 -0300

    Cambios FE-Avance en sección Sobre Mi

---
+++ src/FrontEnd/src/WebPersonal.FrontEnd.Nuxt/components/home/intro.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <NuxtImg src="/avatar.png" alt="Juan Bautista Mangoni"
+        sizes="48px sm:64px"  />
+      <h1 >
+        Hola!
+      </h1>
+      <p>
+      Me llamo Juan Bautista Mangoni y soy desarrollador de software especializado en la plataforma de Microsoft .NET.
+      </p>
+    </div>
+  </template>
+  
+  <script>
+
+  </script>
---
+++ src/FrontEnd/src/WebPersonal.FrontEnd.Nuxt/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <main>
+    <div>
+      <HomeIntro />
+    </div>
+  </main>
+</template>
