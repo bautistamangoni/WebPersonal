f35efd7b9fac2349bffb79c8b11533ad4cf203e7
Bautista Mangoni <juanbautistamangoni@gmail.com>
2024-10-20 11:47:57 -0300

    Cambios FE-Avance en estilos y navbar

--- src/FrontEnd/src/WebPersonal.FrontEnd.Nuxt/app.vue
+++ src/FrontEnd/src/WebPersonal.FrontEnd.Nuxt/app.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <!-- <NuxtRouteAnnouncer />
-    <NuxtWelcome /> -->
+ <div class="section-one">
     <AppNavbar />
+  </div>
+  <div class="section-one2">
     <NuxtPage />
   </div>
 </template>
--- src/FrontEnd/src/WebPersonal.FrontEnd.Nuxt/components/app/navbar.vue
+++ src/FrontEnd/src/WebPersonal.FrontEnd.Nuxt/components/app/navbar.vue
@@ -1,8 +1,24 @@
 <template>
-    <UVerticalNavigation :links="links" />
+
+    <UVerticalNavigation :links="links" 
+    :ui="{
+      // wrapper: 'border-s border-gray-200 dark:border-gray-800 space-y-2',
+      // base: 'group block border-s -ms-px leading-6 before:hidden',
+      padding: 'p-10 ps-4',
+      rounded: '',
+      ring: '',
+      font: 'text-xl', 
+     
+    
+      // active: 'text-primary-500 dark:text-primary-400 border-current font-semibold',
+      // inactive: 'border-transparent hover:border-gray-400 dark:hover:border-gray-500 text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300'
+    }"
+    />
 </template>
 
 <script setup lang="ts">
+import type { _maxWidth } from '#tailwind-config/theme';
+
 const links = [
   [{
     label: 'Juan Bautista Mangoni',
@@ -23,7 +39,7 @@ const links = [
     icon: 'i-heroicons-command-line',
     to: '/components/command-palette'
   }], [{
-    label: 'Ejemplos',
+    label: 'Contacto',
     icon: 'i-heroicons-light-bulb'
   }
   //, {
