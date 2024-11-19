7ff42d803252ca76876f6051f04bbca4e04832c4
bautistamangoni <juanbautistamangoni@gmail.com>
2024-11-18 23:52:38 -0300

    Feat: social links working

--- src/FrontEnd/src/WebPersonal.FrontEnd.Nuxt/pages/social.vue
+++ src/FrontEnd/src/WebPersonal.FrontEnd.Nuxt/pages/social.vue
@@ -4,24 +4,42 @@
       <h1 class="text-xl font-bold tracking-tight text-gray-800">
         Contact Me
       </h1>
-      <!-- <Icon icon="devicon:linkedin-wordmark"  /> -->
-      <UButton
-        icon="devicon:linkedin-wordmark"
-        size="sm"
-        color="primary"
-        variant="solid"
-        label=""
-        to="https://www.linkedin.com/in/juanbautistamangoni/" 
-        external target="_blank"
-        :trailing="false"
-      />
-      <!-- <Icon name="uil:github" style="color: black" /> -->
     </div>
+    <div class="space-y-6 pt-10">
+      <NuxtLink
+      v-for="link in links"
+      :key="link.icon"
+      :to="link.url"
+      target="_blank"
+      class="flex items-end gap-4 dark:hover:text-gray-300 group">
+        <span class="text-sm">
+          {{ link.name }}
+        </span>
+        <div
+          class="flex-1 border-b border-dashed border-gray-300 dark:border-gray-800 group-hover:border-gray-700"
+        ></div>
+        <Icon :name="link.icon" class="w-6 h-6"></Icon>
+      </NuxtLink>
+    </div>
+    
   </main>
 </template>
   
 <script setup>
 // import { Icon } from '@iconify/vue';
+
+  const links = [
+    {
+      name: "GitHub",
+      url: "https://github.com/bautistamangoni",
+      icon: "mdi:github",
+    },
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/juanbautistamangoni/",
+      icon: "mdi:linkedin",
+    }
+  ];
 
   const description =
     "Here you have all my socials to reach me. Looking forward to meet new people and contribute in projects";
